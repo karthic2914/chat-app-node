a87d4d6fcc16a7c537e6f5976d372c549af9cafb
Mahadevan <karthic2914@gmail.com>
2018-01-08 00:50:36 +0530

    Add Connect and disconnect event

--- server/server.js
+++ server/server.js
@@ -1,16 +1,33 @@
 const path = require('path');
-
+const http = require('http');
 const publicPath = path.join(__dirname,'../public');
 const port = process.env.PORT || 3000;
 //call express
 const express = require('express');
-
-var app = express();
-
+//socket io
+const socketIO = require('socket.io');
+const app = express();
 app.use(express.static(publicPath));
 
-//server listen
 
-app.listen(port , () =>{
+
+//use http
+const server = http.createServer(app);
+//use io
+const io = socketIO(server);
+
+//on method is register the an event listener using arrow fuction (Call back)
+io.on('connection',(socket)=>{
+  console.log('New user connected');
+
+  socket.on('connect', () =>{
+    console.log('Connected to Client');
+  });
+  socket.on('disconnect', () =>{
+    console.log('Disconnected from Client');
+  })
+});
+//server listen
+server.listen(port , () =>{
   console.log(`Server is Up on port ${port}` );
 })
